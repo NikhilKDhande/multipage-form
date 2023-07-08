36e0ff6a2dd4a4cb066fc1bb319bb9bb56ec3351
NikhilKDhande <100127292+NikhilKDhande@users.noreply.github.com>
2023-07-08 23:04:54 +0530

    file name changed

--- src/componants/Bottombar.js
+++ src/componants/Bottombar.js
@@ -3,9 +3,12 @@ import React from "react";
 function Bottombar(props) {
   return (
     <div className="btnContainer">
-      <button type="button" className="backBtn" onClick={props.prevStep}>
-        Go Back
-      </button>
+      {props.currentStep > 1 ? (
+        <button type="button" className="backBtn" onClick={props.prevStep}>
+          Go Back
+        </button>
+      ) : null}
+
       {props.currentStep < 4 ? (
         <button type="button" className="nextBtn" onClick={props.nextStep}>
           Next Step
